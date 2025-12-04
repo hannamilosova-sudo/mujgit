bbf6a94d20f5eda7362f3496f49c440409329aa7
Hanna Milosova <hannamilos@seznam.cz>
2025-12-04 22:37:54 +0100

    Commit z lekce 15

--- src/main/helloworld/HelloWorld.py
+++ src/main/helloworld/HelloWorld.py
@@ -1,4 +1,7 @@
+from utils.calculator import soucet
 print("Toto je můj první push po nastavení repozitaře!")
 print("Toto je nová změna")
 print("Hello z baličku helloworld")
 print("Test commit panelu")
+print(soucet(2, 3))
+print("Commit z lekce 15")
